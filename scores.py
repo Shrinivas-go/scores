23f0a7bedb69742fc7761fcca57dc351567afe19
Shrinivas-go <shrinivas4927@gmail.com>
2026-01-13 14:44:35 +0530

    saving

--- scores.py
+++ scores.py
@@ -6,10 +6,10 @@ if len(sys.argv) == 4:
     for i in range(1,4):
       score.append(int(sys.argv[i]))
 
-    total = score[0] + score[1] + score[2]
+    sum = score[0] + score[1] + score[2]
     average = total / len(score)
 
-    print(f"Sum: {total}")
+    print(f"Sum: {sum}")
     print(f"Average: {average}")
-    print(f"Minimum: {min(average)}")
-    print(f"Maximum: {max(average)}")
+    print(f"Minimum: {min(score)}")
+    print(f"Maximum: {max(score)}")
